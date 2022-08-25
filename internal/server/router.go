c19c233f5d2747a00bdc33a0d71fbbc013bb8726
Tuan Luu <tuan.luu@asnet.com.vn>
2022-08-25 16:32:07 +0700

    Adding User

--- internal/server/router.go
+++ internal/server/router.go
@@ -9,13 +9,15 @@ func setRouter() *gin.Engine {
 	// Creates default gin router with Logger and Recovery middleware already attached
 	router := gin.Default()
 
+	// Enables automatic redirection if the current route can't be matched but a
+	// handler for the path with (without) the trailing slash exists.
+	router.RedirectTrailingSlash = true
+	
 	// Create API route group
 	api := router.Group("/api")
 	{
-		// Add /hello GET route to router and define route handler function
-		api.GET("/hello", func(ctx *gin.Context) {
-			ctx.JSON(200, gin.H{"msg": "world"})
-		})
+		api.POST("/signup", signUp)
+		api.POST("/signin", signIn)
 	}
 
 	router.NoRoute(func(ctx *gin.Context) {
